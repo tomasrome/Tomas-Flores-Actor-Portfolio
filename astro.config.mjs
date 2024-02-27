0ce11c2879aff5d3423d80f96b86c756da11fe75
TomasRom <tomasrom.dev@gmail.com>
2024-02-27 20:44:32 -0300

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,5 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "tomasrome.github.io.",
-  base: "/Tomas-Flores-Actor-Portfolio",
+  site: "https://tomasrome.github.io",
 });
