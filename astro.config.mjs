edcb6e0bbe70f4d9e04193e34cbd3ff0699547e2
TomasRom <tomasrom.dev@gmail.com>
2024-07-03 11:40:17 -0300

    gallery from jpg to webp

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,9 @@
 import { defineConfig } from "astro/config";
+import sitemap from "astro-sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://tomasrome.github.io",
   base: "/Tomas-Flores-Actor-Portfolio",
+  integrations: [sitemap()],
 });
